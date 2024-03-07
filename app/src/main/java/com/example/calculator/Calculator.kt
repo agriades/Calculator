b3c191385eda5564ce4572aea88f63c4d95df09d
Sooyoung Hwang <agriades23@gmail.com>
2024-03-07 20:36:36 +0900

    Calculator class 속 사칙연산 메소드를 MainActivity.kt 에서 불러오는 코드는 작성함. 에러는 없게 수정했는데, 테스트할 방법을 모르겠음. 앱 실행 시 자꾸 강제종료가 되기 때문임.

---
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -0,0 +1,10 @@
+package com.example.calculator
+
+class Calculator() {
+
+    fun add(currentTotal : Float, valueToAdd: Float) = currentTotal + valueToAdd
+    fun subtract (currentTotal : Float, valueToSubtract: Float) = currentTotal - valueToSubtract
+    fun multiply (currentTotal: Float, valueToMultiply: Float) = currentTotal * valueToMultiply
+    fun divide (currentTotal: Float, valueToDivide: Float) = currentTotal / valueToDivide
+
+}
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,5 +7,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculator = Calculator() // 계산기 불러와! Calculator 클래스의 인스턴스 생성
+
+        var currentResult: Float = 0f
+        var answer: Float = 0f
+        var isItOkay: Boolean = true
+
+
+        while (isItOkay) {
+            //입력값 받기
+            println("숫자 입력: ")
+            val currentInput = readLine()!!.toFloat()
+            print("연산자를 입력하세요: 더하기(+), 빼기(-), 곱하기(*), 나누기(/): ")
+            val operation = readLine()
+
+            when (operation) {
+                "더하기", "+" -> answer = calculator.add(currentResult, currentInput)
+                "빼기", "-" -> answer = calculator.subtract(currentResult, currentInput)
+                "곱하기", "*" -> answer = calculator.multiply(currentResult, currentInput)
+                "나누기", "/" -> answer = calculator.divide(currentResult, currentInput)
+                else -> isItOkay = false
+            }
+
+            println("지금까지 계산한 결과는 ${answer}입니다.")
+            currentResult = answer
+        }
     }
 }
